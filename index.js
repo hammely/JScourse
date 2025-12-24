41c70fec13e80c8f0a9a09e32df903d034c43891
Fomina Lisa <elizadet020@gmail.com>
2025-12-24 13:18:12 +0300

    Task 13

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-const name = 'Лиза';
-const age = 21;
-const userProfile = {
-  name,    // вместо name: name
-  age,     // вместо age: age
-  isStudent: true,
-  hobby: "painting"
-};
-console.log(userProfile);
-
+const myCity = {
+    city: 'My city',
+    greeting() {                 // вместо greeting: function() {...}
+      console.log('Greetings!');
+    }
+  };
+  myCity.greeting(); // вызов метода
