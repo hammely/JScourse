09f91dd49246eedb8f7984c8f41bd157b17cb4d2
Fomina Lisa <elizadet020@gmail.com>
2025-12-24 13:10:22 +0300

    Task 8

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const userName = 'Лиза'; // const — значение не изменится
-let userAge = 25;         // let — можно изменить позже
-userAge = 21;              // изменяем значение
-
-console.log(userName, userAge);
+const city = {
+    name: 'My city',
+    population: 8000000
+  };
+  
+  city.name = 'Dzerzhinsk';      // изменение свойства
+  city.country = 'RUSSIA';         // добавление свойства
+  delete city.population;       // удаление свойства
+  
+  console.log(city);
