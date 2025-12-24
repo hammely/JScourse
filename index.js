598f934f8aee93176046f5be55c304999a49cabd
Fomina Lisa <elizadet020@gmail.com>
2025-12-24 14:27:43 +0300

    Task 65

--- index.js
+++ index.js
@@ -1,25 +1,5 @@
-let month = 11;
-switch (month) {
-    case 12:
-    case 1:
-    case 2:
-        console.log("Зима");
-        break;
-    case 3:
-    case 4:
-    case 5:
-        console.log("Весна");
-        break
-    case 6:
-    case 7:
-    case 8:
-        console.log("Лето");
-        break
-    case 9:
-    case 10:
-    case 11:
-        console.log("Осень");
-        break
-    default:
-        console.log("Такого месяца не существует");
-}
+let i = 0;
+do {
+  console.log(i); // 0
+  i++;
+} while (i < 3);
