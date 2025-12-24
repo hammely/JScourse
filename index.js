9a52c85afca06de27f62d1050bdbe42eb26bd0cd
Fomina Lisa <elizadet020@gmail.com>
2025-12-24 13:06:54 +0300

    Task 6

---
+++ index.js
@@ -0,0 +1,5 @@
+const userName = 'Лиза'; // const — значение не изменится
+let userAge = 25;         // let — можно изменить позже
+userAge = 21;              // изменяем значение
+
+console.log(userName, userAge);
