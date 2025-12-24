150225b380aed493bfe6937e2024c74e5d8f8580
Fomina Lisa <elizadet020@gmail.com>
2025-12-24 14:02:59 +0300

    Task 37

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-// Declaration отдельная функция
-console.log(add(5, 3)); // 8
-function add(x, y) { return x + y; }
-
-// Expression константа как функция
-const sub = function(x, y) { return x - y; };
-console.log(sub(5, 3)); // 2
-
-// Стрелочная функция (разновидность Expression)
-const div = (x, y) => x / y;
-console.log(div(6, 2)); // 3
+function multiply(value, multiplier = 1) {
+    return value * multiplier;
+}
+console.log(multiply(5)); // 5
