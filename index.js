d5c40c969300ba744ae18b42120213cafab28583
Fomina Lisa <elizadet020@gmail.com>
2025-12-24 14:25:29 +0300

    Task 60

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-function sumPositiveNums(a, b) {
-    if (typeof a !== "number" || typeof b !== "number") {
-      return "One of the arguments is not a number"; // Проверка числового формата
-    }
-    if (a <= 0 || b <= 0) { // Проверка чисел на неотрицательность 
-      return "Numbers are not positive";
-    }
-    return a + b;
-  }
-  console.log(sumPositiveNums(-10, 8)); // 11
+let month = 11;
+switch (month) {
+    case 12:
+    case 1:
+    case 2:
+        console.log("Зима");
+        break;
+    case 3:
+    case 4:
+    case 5:
+        console.log("Весна");
+        break
+    case 6:
+    case 7:
+    case 8:
+        console.log("Лето");
+        break
+    case 9:
+    case 10:
+    case 11:
+        console.log("Осень");
+        break
+    default:
+        console.log("Такого месяца не существует");
+}
