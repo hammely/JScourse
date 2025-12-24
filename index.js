1c683b4f015f5b13b0aac932bc939b5cc2df74d1
Fomina Lisa <elizadet020@gmail.com>
2025-12-24 14:18:22 +0300

    Task 56

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-// Правильно
-const arr = [1, 2, 3];
-const result = arr.map((x) => x * 3);
-console.log(result); // [3, 6, 9]
-
-// Ошибка: нет return
-const wrong = arr.map((x) => { x * 3; });
-console.log(wrong); // [undefined, undefined, undefined]
+const userInfo = ({ name, commentQuantity }) => {
+    if (!commentQuantity) return `User ${name} has no comments`; // Нет комментариев
+    return `User ${name} has ${commentQuantity} comments`;
+  };
+  const profile = { name: "Лиза", commentQuantity: 100 };
+  console.log(userInfo(profile));
