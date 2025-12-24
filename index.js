5de6af2325a965ad298104798e35263ca75b98e8
Fomina Lisa <elizadet020@gmail.com>
2025-12-24 14:30:49 +0300

    Task 68

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-const myObject = { x: 10, y: true, z: "bc" };
-Object.keys(myObject).forEach(key => console.log(key, myObject[key]));
-Object.values(myObject).forEach(value => console.log(value));
+const str = "abc";
+for (const letter of str) {
+  console.log(letter); // "a", "b", "c"
+}
+const arr = [true, 10, "bc"];
+for (const element of arr) {
+  console.log(element); // true, 10, "bc"
+}
